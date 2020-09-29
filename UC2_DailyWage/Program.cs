5ad65bfe1d642533a239132496a8ae59d174623b
Kirti Swaraj <Sai@KirtiSwaraj>
2020-09-29 22:20:38 +0530

    [kirti]Add .Added Daily Wage

---
+++ UC2_DailyWage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UC2_DailyWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int random_Num = r.Next(0, 2);
+            string emp_Check = "";
+            int f_T_WorkingHours = 8;
+            int wagePerHour = 20;
+            if (random_Num == 1)
+            {
+                emp_Check = "Present";
+                Console.WriteLine("Employee is {0}", emp_Check);
+                Console.WriteLine("Daily wage of employee is {0}", f_T_WorkingHours * wagePerHour);
+
+            }
+            else
+            {
+                emp_Check = "Absent";
+                Console.WriteLine("Employee is {0}", emp_Check);
+            }
+
+        }
+    }
+}
