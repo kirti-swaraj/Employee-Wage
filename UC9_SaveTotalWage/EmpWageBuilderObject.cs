442643b1689c63410e87c8cd2bc25706ad543e05
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-01 00:30:16 +0530

    [kirti]Add. SaveTotalWage

---
+++ UC9_SaveTotalWage/EmpWageBuilderObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC9_SaveTotalWage
+{
+    class EmpWageBuilderObject
+    {
+        private string company;
+        private int empRatePerHour, numOfWorkingDays, maxWorkingHours;
+        private int totalEmpWage;
+
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHours)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxWorkingHours = maxWorkingHours;
+            this.totalEmpWage = 0;
+        }
+        const int IS_ABSENT = 0;
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+
+        public void CalculateEmpWage()
+        {
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("Wage Calculation for Employee of " + this.company);
+            Console.WriteLine("*****************************************************");
+            Random randObj = new Random();
+            int empHours = 0, totalWorkingHoursCumulative = 0;
+
+            for (int currentDay = 1; currentDay <= numOfWorkingDays && totalWorkingHoursCumulative <= maxWorkingHours; currentDay++)
+            {
+                int option = randObj.Next(0, 3);
+                switch (option)
+                {
+                    case IS_ABSENT:
+                        empHours = 0;
+                        break;
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHours = 8;
+                        break;
+                    default:
+                        break;
+                }
+                if (totalWorkingHoursCumulative + empHours > maxWorkingHours)
+                    empHours = 0;
+                totalWorkingHoursCumulative += empHours;
+                Console.WriteLine("Day " + (currentDay) + " Employee Hours " + empHours);
+            }
+            Console.WriteLine("Total Employee Hours " + totalWorkingHoursCumulative);
+            totalEmpWage = totalWorkingHoursCumulative * empRatePerHour;
+            Console.WriteLine("Total Employee Wage " + totalEmpWage + "\n\n");
+        }
+
+        public void GetDetails()
+        {
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("Total Employee Wage for " + company + " Employee is: " + totalEmpWage);
+            Console.WriteLine("***************************************************** \n");
+        }
+    }
+}
---
+++ UC9_SaveTotalWage/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        const int IS_ABSENT = 0;
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+
+        static void Main(string[] args)
+        {
+            CalculateEmpWage("Sap Labs", 200, 20, 100);
+            CalculateEmpWage("DE Shaw", 250, 20, 70);
+        }
+        static void CalculateEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHours)
+        {
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("Wage Calculation for Employee of " + company);
+            Console.WriteLine("*****************************************************");
+            Random randObj = new Random();
+            int empHours = 0, totalWorkingHoursCumulative = 0;
+
+            for (int currentDay = 1; currentDay <= numOfWorkingDays && totalWorkingHoursCumulative <= maxWorkingHours; currentDay++)
+            {
+                int option = randObj.Next(0, 3);
+                switch (option)
+                {
+                    case IS_ABSENT:
+                        empHours = 0;
+                        break;
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHours = 8;
+                        break;
+                    default:
+                        break;
+                }
+                if (totalWorkingHoursCumulative + empHours > maxWorkingHours)
+                    empHours = 0;
+                totalWorkingHoursCumulative += empHours;
+                Console.WriteLine("Day " + (currentDay) + " Employee Hours " + empHours);
+            }
+            Console.WriteLine("Total Employee Hours " + totalWorkingHoursCumulative);
+            int totalEmpWage = totalWorkingHoursCumulative * empRatePerHour;
+            Console.WriteLine("Total Employee Wage: " + totalEmpWage + "\n\n");
+            // State of the object can be stored using serialization of objects
+            EmpWageBuilderObject SapLabsWageBuilder = new EmpWageBuilderObject("Sap Labs", 200, 20, 100);
+            EmpWageBuilderObject DEShawWageBuilder = new EmpWageBuilderObject("DE Shaw", 250, 20, 70);
+            SapLabsWageBuilder.CalculateEmpWage();
+            DEShawWageBuilder.CalculateEmpWage();
+            SapLabsWageBuilder.GetDetails();
+            DEShawWageBuilder.GetDetails();
+        }
+
+
+    }
+}
