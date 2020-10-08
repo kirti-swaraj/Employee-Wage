02b183b851092fdd458738db2355451eac325716
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-08 06:36:56 +0530

    [kirti]Add . Query by company

---
+++ EmpWageBuilder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblem
+{
+    public class EmpWageBuilder: IComputeWage
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        private LinkedList<CompanyEmpWage> companyEmpWagesList;
+        private Dictionary<string, CompanyEmpWage> companyToEmpWageMap;
+        public EmpWageBuilder()
+        {
+            this.companyEmpWagesList = new LinkedList<CompanyEmpWage>();
+            this.companyToEmpWageMap = new Dictionary<string, CompanyEmpWage>();
+        }
+        public void AddCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            this.companyEmpWagesList.AddLast(companyEmpWage);
+            this.companyToEmpWageMap.Add(company, companyEmpWage);
+        }
+        public void ComputeEmpWage()
+        {
+            foreach (CompanyEmpWage companyEmpWage in this.companyEmpWagesList)
+            {
+                int totalEmpWageTemp = this.ComputeEmpWage(companyEmpWage);
+                companyEmpWage.settotalEmpWage(totalEmpWageTemp);
+                Console.WriteLine(companyEmpWage.toString());
+            }
+        }
+        public int ComputeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            int totalEmpHours = 0;
+            int workingDays = 0;
+            int empHours = 0;
+            int totalWagePerDay = 0;
+            int totalWagePerMonth = 0;
+            Console.WriteLine("Company:" + companyEmpWage.company + "=>");
+            while (totalEmpHours < companyEmpWage.maxHoursPerMonth && workingDays < companyEmpWage.numOfWorkingDays)
+            {
+                EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+                empHours = empWageBuilder.GetWorkingHours();
+                if (totalEmpHours == companyEmpWage.maxHoursPerMonth - 4)
+                {
+                    empHours = 4;
+                }
+                if (empHours != 0)
+                {
+                    workingDays++;
+                    totalEmpHours = empHours + totalEmpHours;
+                    totalWagePerDay = empHours * companyEmpWage.ratePerHours;
+                    totalWagePerMonth += totalWagePerDay;
+                    Console.WriteLine("Total Wage per Day.." + totalWagePerDay);
+                }
+            }
+            return totalWagePerMonth;
+        }
+        public int GetWorkingHours()
+        {
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            int empHours = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case FULL_TIME:
+                    empHours = 8;
+                    break;
+                case PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+                    break;
+            }
+            return empHours;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.AddCompanyEmpWage("Dmart", 20, 10, 60);
+            empWageBuilder.AddCompanyEmpWage("Reliance", 15, 14, 40);
+            empWageBuilder.ComputeEmpWage();
+            Console.WriteLine("\nTotal wage for Dmart is: " + empWageBuilder.GetTotalWage("Dmart"));
+            Console.ReadKey();
+        }
+    }
+
+
+             public interface IComputeWage
+        {
+            void AddCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+
+            void ComputeEmpWage();
+            int GetTotalWage(string company);
+
+        } 
+    }
+   
+    
+
