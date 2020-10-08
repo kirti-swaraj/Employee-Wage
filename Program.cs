679e7d33ce23a3467924d9bb44ce364b3f78316f
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-08 06:34:19 +0530

    [kirti]Add. Store Daily Wage

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.AddCompanyEmpWage("Dmart", 20, 10, 60);
+            empWageBuilder.AddCompanyEmpWage("Reliance", 15, 7, 40);
+            empWageBuilder.ComputeEmpWage();
+            Console.ReadKey();
+        }
+    }
+}
